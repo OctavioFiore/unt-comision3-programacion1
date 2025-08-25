22ce88aee192a4274a01048ee568401a66e6bd9c
OctavioFiore <fioreoctavio6@gmail.com>
2025-08-25 19:01:58 -0300

    Intentando arreglar lo que intente hacer en clase

---
+++ 00 Trabajos practicos,  aparte de las unidadas/Trabajo de secuenciales en clase.py
@@ -0,0 +1,60 @@
+fecha=input("ingrese la fecha actual en formato “día,DD/MM”: " )
+dia_semana,fecha_en_numeros=fecha.split(",")
+
+print(dia_semana)
+dia,mes=fecha_en_numeros.split("/")
+
+print(dia)
+
+print(mes)
+
+dia_semana=dia_semana.lower()
+
+"""
+if dia_semana="lunes":
+    Print("Se dicta nivel inicial")
+elif dia_semana="martes":
+    print("Se dicta clase nivel intermedio")
+elif dia_semana="Miercoles":
+    print("Se dicta clase nivel avanzado") 
+elif dia_semana="jueves ":
+    print("Se dicta clase para práctica hablada ")
+elif dia_semana="viernes":
+    print("Se dicta inglés para viajero")  
+
+
+if  dia<=31 and mesa<=12:
+else
+print("Error la fecha es incorrecta")
+
+
+alumnon_aprobados=int(input("Ingrese la cantidad de alumnos aprobados"))
+
+examen=input("Hubo examen")
+examen=examen.lower()
+if examen="si":
+    aprobados(int("Ingrese la cantidad de alumnos aprobados"))
+    desaprobados(int("Ingrese la cantidad de alumnos desaprobados")
+    total=aprobados+desaprobados
+    porcentaje=(aprobados/total)+100
+    print(f'Porcentajes de aprobados es:¨{porcentaje}")
+elif examen="no":
+    prin("No hubo examen")
+else
+    print("Es si o no hubo examen")
+
+
+
+if dia_semana = "jueves":
+    asistencia(input("Ingrese el porcentaje de asistencia a clase: "))
+else
+if asistencia>=50:
+    print("asistió la mayoría")
+else
+    print("no asistió la mayoría")
+
+
+if dia = 1 and (mes>1= or mes>=7):
+    print("Comienzo de nuevo ciclo")
+    cantidad_alumnos=int(input("Ingrese la cantidad de alumnos y el arancel
+"""
